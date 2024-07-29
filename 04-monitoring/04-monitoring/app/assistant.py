2b3ea7616d133e37da5f878d578fce548530b18b
Sagar Dhal <sagar.dhal@outlook.com>
2024-07-29 12:25:02 +0000

    updatd with localhost

--- 04-monitoring/04-monitoring/app/assistant.py
+++ 04-monitoring/04-monitoring/app/assistant.py
@@ -10,7 +10,7 @@ from sentence_transformers import SentenceTransformer
 
 ELASTIC_URL = os.getenv("ELASTIC_URL", "http://elasticsearch:9200")
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://ollama:11434/v1/")
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "mykey")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-None-rdW2gIwMXCqNMpURssa7T3BlbkFJOZRYwhaEFmeT4BqNpe1l")
 
 
 es_client = Elasticsearch(ELASTIC_URL)
